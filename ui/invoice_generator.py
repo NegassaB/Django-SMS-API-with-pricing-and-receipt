4fe3f7a1ce6e62c5e586bc6193b2979a61f8885f
Negassa Berhanu <negassab16@gmail.com>
2019-12-21 17:25:17 +0300

    Finally works, with Jalex right next 2 me
    The invoice generator finally works, what's left is to convert it to
    pdf, save it to user, run it every month and security.
    
    Hojiin Itti Fufaa.

--- ui/invoice_generator.py
+++ ui/invoice_generator.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from commons.models import SMSUser, SMSMessages, Invoice
 from commons.serializers import InvoiceSerialzer
 
+from datetime import datetime, date
+
 """
 don't know how to do it yet, but it should:
 1. get the company from the SMSMessages table
@@ -27,20 +29,33 @@ def generate_invoice(request, username, template_name):
     invoice_number = user_invoice.pk
     paid_status = user_invoice.payment_status
     #TODO use something else other than a dictionary that can hold the username, the company and the total sent by that user in the month
-    all_sms_sent = {}
+    all_sms_sent_by_users = {}
     for user in all_users_in_company:
-        #TODO add the month that needs to be billed to this filter parameter
         #TODO you can get the month automatically by calling datetime.now.month()
         # TODO you can run the code at the begining of each month by doing:
         # TODO x = date.today() then x = x.replace(day=1) 
-        all_sms_sent[user] = SMSMessages.objects.filter(
+        total_sent_by_single_user = SMSMessages.objects.filter(
             sending_user=user,
             delivery_status="True",
-            sent_date__year="2019",
-            sent_date__month = "10"
+            sent_date__year=datetime.now().year,
+            sent_date__month = datetime.now().month - 2
             )
-    # total_amount_sent = all_sms_sent.count()
-
+        all_sms_sent_by_users = {
+            "all_sms_sent": [
+                {
+                "user": user,
+                "total_sent_single_user": total_sent_by_single_user.count(),
+                "company_name": company_to_invoice
+                }
+            ]
+        }
+    total_amount_sent = SMSMessages.objects.filter(
+        sent_date__year = datetime.now().year,
+        sent_date__month = 10,
+        # sent_date__month = datetime.now().month - 1,
+        sending_user__company_name=company_to_invoice
+    )
+    
     ret_val = render(
         request,
         template_name=template_name,
@@ -51,12 +66,11 @@ def generate_invoice(request, username, template_name):
             "invoice_number": invoice_number,
             "tin": company_tin,
             "email": user_email,
-            "bill_month": "not yet defined",
-            "due_date": "not yet defined",
-            "all_users_that_sent_sms": all_users_in_company,
-            "total_amount_sent": "not yet defined",
-            "vat": "not yet defined",
-            "total_price": "not yet defined",
+            "bill_month": datetime.now().month - 1,
+            "all_sms_sent": all_sms_sent_by_users,
+            "total_amount_sent": total_amount_sent.count(),
+            "vat": total_amount_sent.count() * 0.70 * 0.15,
+            "total_price": total_amount_sent.count() * 0.70 * 1.15, 
             "payment_status": paid_status,
         }
         )
