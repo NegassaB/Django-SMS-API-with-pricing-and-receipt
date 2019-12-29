e77e6a24b4222f1d96c398ffdd73543edec1bb29
Negassa Berhanu <negassab16@gmail.com>
2019-12-29 17:46:25 +0300

    removed the commented lines from logoutscript.js

--- ui/static/ui/js/logoutscript.js
+++ ui/static/ui/js/logoutscript.js
@@ -6,35 +6,4 @@ $("#logout_button").click(function(){
             window.location.href = url;
         },
     );
-}); 
-
-// $("#logout_button").click(
-//     function() {
-//         $.get(
-//             "http://localhost:8055/ui/logout/",
-//             function(data) {
-//                 window.alert(data);
-//                 window.location.href.replace(data.redirect_url);
-//             }
-//         );
-//         }
-// );
-
-// function getData(){
-//     url = "http://localhost:8055/ui/ajax/dashboard_update/";
-//     $.post(
-//         url,
-//         {
-//             ajaxUserToken: $('#ajaxUserToken').val(),
-//             csrfmiddlewaretoken: $('#ajaxCsrfToken').val()
-//         },
-//         function(data){
-//             if (data.total_msgs != 0 || data.total_msgs != null) {
-//                 // alert("got a response: " + data.total_msgs + "\nStatus: " + status)
-//                 $("#ajaxTotalSent").text(data.total_msgs);
-//             }
-//             if(data.last5_sent !=0 || data.last5_sent != null) {
-//                 $("#ajaxLast5minSent").text(data.last5_sent)
-//             }
-//         },
-//         "json");
+});
