e395614bf684b7fbf36980b1bdff7696f87d5145
NegassaB <negassab16@gmail.com>
2023-07-06 15:05:07 +0200

    update generator to accept year and month from user

--- commons/invoice_generator.py
+++ commons/invoice_generator.py
@@ -2,19 +2,18 @@ from commons.models import SMSUser, SMSMessages
 import datetime
 
 
-def generator(month_num=None):
+def generator(year=None, month=None):
     current_datetime = datetime.datetime.now()
-    year = current_datetime.year
-    if month_num:
-        month = current_datetime.month - month_num
-    else:
+    if not year:
+        year = current_datetime.year
+    if not month:
         month = current_datetime.month
     all_users = SMSUser.objects.all()
     for user in all_users:
         sent_sms = SMSMessages.objects.filter(sending_user=user, sent_date__year=year, sent_date__month=month)
         if sent_sms.count() == 0:
             continue
-        if user.username == "ActivBetUser1" or user.username == "UTopUser":
+        if user.username == "ActivBetUser1" or user.username == "UTopUser" or user.username == "ChampionBet":
             sent_sms = sent_sms.filter(delivery_status=True)
         print(f"sending_user -- {user.username} sent_sms {sent_sms.count()}")
 
