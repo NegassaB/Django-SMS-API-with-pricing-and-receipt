9a8a1d5a3eea1b76651e8185327a28b59d852356
Negassa Berhanu <negassab16@gmail.com>
2025-08-09 14:38:22 +0300

    Update serializers.py

--- notification/serializers.py
+++ notification/serializers.py
@@ -5,14 +5,7 @@ from rest_framework import serializers
 from django.core.validators import RegexValidator
 
 
-PHONE_VALIDATOR = RegexValidator(
-    r"^(\+2517|2517|2519|07|09|\+2519)([0-9]{8})$",
-    "The phone number is invalid",
-    code="invalid",
-)
-
-
 class PhoneNumberSerializer(serializers.Serializer):
     sms_number_to = serializers.CharField(
-        max_length=13, min_length=13, initial="+251", validators=[PHONE_VALIDATOR]
+        max_length=13, min_length=13, initial="+251", validators=[RegexValidator]
     )
